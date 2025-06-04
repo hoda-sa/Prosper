9fe9612981735ca350aa6d45baa6ef7142e7fcff
Hoda Aghaei <hoda.s.aghaei@gmail.com>
2025-06-04 08:43:30 -0700

    small correction in plaid api

--- server/config/plaid.js
+++ server/config/plaid.js
@@ -7,7 +7,7 @@ const configuration = new Configuration({
         headers: {
             'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
             'PLAID-SECRET': process.env.PLAID_SECRET,
-            'Plaid-Version': '2020-09-14', 
+            'Plaid-Version': '2020-09-14',
         },
     },
 });
@@ -16,7 +16,7 @@ const plaidClient = new PlaidApi(configuration);
 
 // Plaid products and country codes (required for link token)
 const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || 'transactions,accounts').split(',');
-const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || ['US', 'CA']).split(',');
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'CA');
 
 // Validate required environment variables
 const requiredEnvVars = ['PLAID_CLIENT_ID', 'PLAID_SECRET'];
